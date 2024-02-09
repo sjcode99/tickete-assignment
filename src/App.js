58a3eb2e8a02e1b710993e456f4ed964530c41db
sjcode99 <misuraj2611@gmail.com>
2024-02-09 20:02:25 +0530

    App.js file updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import HeaderComponent from "./components/HeaderComponent";
+import HeaderTimerComponent from "./components/HeaderTimerComponent";
+import LeftComponent from "./components/LeftComponent";
+import RightComponent from "./components/RightComponent";
+import FaqComponent from "./components/FaqComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HeaderComponent />
+        <HeaderTimerComponent />
       </header>
+
+      <div style={{ padding: "0 6.5rem" }}>
+        <section className="content-box">
+          <div className="inner-content-box">
+            <LeftComponent />
+            <RightComponent />
+          </div>
+        </section>
+
+        <hr />
+        <section className="faq-section">
+          <FaqComponent></FaqComponent>
+        </section>
+
+        <hr />
+      </div>
+      <footer>
+        <FooterComponent />
+      </footer>
     </div>
   );
 }
