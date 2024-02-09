cfe78fdff00d9e540eb5e45fef2dfb14cc812101
sjcode99 <misuraj2611@gmail.com>
2024-02-09 19:59:22 +0530

    header and header timer section created

---
+++ src/components/HeaderTimerComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderTimerComponent = () => {
+  return (
+    <div className='header-timer'>
+        <p className='py-2'>Holding your tickets for 30:00</p>
+    </div>
+  )
+}
+
+export default HeaderTimerComponent
