4d0b449ff9ec54f6f94e08bbfe5e649ceca9cc92
sjcode99 <misuraj2611@gmail.com>
2024-02-10 19:36:55 +0530

    responsively handled for mobile

---
+++ src/components/Header/TimerComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const TimerComponent = () => {
+  return (
+    <div className='header-timer'>
+        <p className='py-2'>Holding your tickets for 30:00</p>
+    </div>
+  )
+}
+
+export default TimerComponent
